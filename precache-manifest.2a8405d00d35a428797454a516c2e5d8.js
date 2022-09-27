d185c5c7cb9eea491d70148b1eef3e198565e791
Moin <moin.sha55ikh@gmail.com>
2022-09-27 17:23:10 +0530

    Updates

---
+++ precache-manifest.2a8405d00d35a428797454a516c2e5d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f632acc3adc82fbf6451",
+    "url": "/Text-UtiL-Ls/static/css/main.ccd1a1a7.chunk.css"
+  },
+  {
+    "revision": "f632acc3adc82fbf6451",
+    "url": "/Text-UtiL-Ls/static/js/main.33384596.chunk.js"
+  },
+  {
+    "revision": "d4f06140e1a00db9df37",
+    "url": "/Text-UtiL-Ls/static/js/runtime~main.8e142548.js"
+  },
+  {
+    "revision": "332a33717a9fb4426c5b",
+    "url": "/Text-UtiL-Ls/static/js/2.7331aea6.chunk.js"
+  },
+  {
+    "revision": "db569b567d12d48e4e61",
+    "url": "/Text-UtiL-Ls/static/js/3.d51c813c.chunk.js"
+  },
+  {
+    "revision": "2a1071d90e66af54c69e9f20ee45b25c",
+    "url": "/Text-UtiL-Ls/index.html"
+  }
+];
